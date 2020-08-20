a85dd9bbf1b6951b70863417e2e8d66dc0ecaf01
Cheng Li <cli99@illinois.edu>
2020-08-20 00:45:53 -0700

    refactor

---
+++ src/hypermodern_python/console.py
@@ -0,0 +1,19 @@
+import click
+
+import textwrap
+
+from . import __version__, wikipedia
+
+
+@click.command()
+@click.version_option(version=__version__)
+def main():
+    """The hyermodern Python project.
+    """
+    data = wikipedia.random_page()
+
+    title = data["title"]
+    extract = data["extract"]
+
+    click.secho(title, fg="green")
+    click.echo(textwrap.fill(extract))
