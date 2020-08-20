f3fcaa553dcdc0ad8b5640271f16fa606736733f
Cheng Li <cli99@illinois.edu>
2020-08-20 01:24:47 -0700

    add language selection

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -1,3 +1,4 @@
+from typing import Collection
 import click
 
 import textwrap
@@ -6,11 +7,19 @@ from . import __version__, wikipedia
 
 
 @click.command()
+@click.option(
+    "--language",
+    "-l",
+    default="en",
+    help="Language edition of Wikipedia",
+    metavar="LANG",
+    show_default=True,
+)
 @click.version_option(version=__version__)
-def main():
+def main(language):
     """The hyermodern Python project.
     """
-    data = wikipedia.random_page()
+    data = wikipedia.random_page(language=language)
 
     title = data["title"]
     extract = data["extract"]
