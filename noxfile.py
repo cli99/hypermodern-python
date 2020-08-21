7a5aee52f9a5c1cee1a5023f3e7b5544a6d68b47
Cheng Li <cli99@illinois.edu>
2020-08-20 22:32:50 -0700

    add safety to check insecure package

--- noxfile.py
+++ noxfile.py
@@ -30,3 +30,22 @@ def lint(session):
         "flake8-bandit",
     )
     session.run("flake8", *args)
+
+
+import tempfile
+
+
+@nox.session(python="3.8")
+def safety(session):
+    with tempfile.NamedTemporaryFile() as requirements:
+        session.run(
+            "poetry",
+            "export",
+            "--dev",
+            "--format=requirements.txt",
+            "--without-hashes",
+            f"--output={requirements.name}",
+            external=True,
+        )
+        session.install("safety")
+        session.run("safety", "check", f"--file={requirements.name}", "--full-report")
