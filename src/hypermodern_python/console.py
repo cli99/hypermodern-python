49ec95dd4c3600f71497f678a04531bfc1f15f78
Cheng Li <cli99@illinois.edu>
2020-08-21 16:20:22 -0700

    add data validation using desert and marshmallow

--- src/hypermodern_python/console.py
+++ src/hypermodern_python/console.py
@@ -23,10 +23,7 @@ from . import __version__, wikipedia
 def main(language: str) -> None:
     """The hyermodern Python project.
     """
-    data = wikipedia.random_page(language=language)
+    page = wikipedia.random_page(language=language)
 
-    title = data["title"]
-    extract = data["extract"]
-
-    click.secho(title, fg="green")
-    click.echo(textwrap.fill(extract))
+    click.secho(page.title, fg="green")
+    click.echo(textwrap.fill(page.extract))
